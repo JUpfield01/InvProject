0cd6dcb0a7335e70490113eefc8e171401f67569
jupfield76692 <jupfield.76692@farnborough.ac.uk>
2018-12-14 14:27:28 +0000

    Fixed

--- src/server/controllers/LoginController.java
+++ src/server/controllers/LoginController.java
@@ -1,10 +1,7 @@
 package server.controllers;
 
-import server.Logger;
-import server.models.User;
-import server.models.services.UserService;
+public class LoginController {
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import java.util.UUID;
+}
+
 
